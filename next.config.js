e65c04bfe8126f0d6fd3db52c1671d7bac058da4
r_gao <r_gao@trip.com>
2021-08-19 19:19:46 +0800

    先禁掉分析插件

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const withPlugins = require('next-compose-plugins');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true'
-});
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//     enabled: process.env.ANALYZE === 'true'
+// });
 
 module.exports = withPlugins(
     [
-        [withBundleAnalyzer]
+        // [withBundleAnalyzer]
     ],
     {
         basePath: '/simple-h5',
